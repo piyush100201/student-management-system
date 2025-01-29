c52c8e44f22073cbba98edbe0672576e57bd4b93
Piyush Jain <piyushjain100201@gmail.com>
2025-01-29 21:51:13 +0530

    Search Student

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sqlite3
 
 from datetime import datetime
 
+from PyQt6.QtCore import Qt
 from PyQt6.QtGui import QAction
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, \
     QLineEdit, QPushButton, QComboBox, QMainWindow, QTableWidget, QTableWidgetItem, QDialog
@@ -13,9 +14,16 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Student Management System")
 
+
+        #Add menu items
         file_menu_item = self.menuBar().addMenu("&File")
+
         help_menu_item = self.menuBar().addMenu("&Help")
 
+        edit_menu_item = self.menuBar().addMenu("&Edit")
+
+
+        #Add actions
         add_student_action = QAction("Add Student", self)
         add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
@@ -23,6 +31,11 @@ class MainWindow(QMainWindow):
         about_action = QAction("About", self)
         help_menu_item.addAction(about_action)
 
+        search_action = QAction("Search", self)
+        edit_menu_item.addAction(search_action)
+        search_action.triggered.connect(self.search)
+
+        #Add table
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id","Name","Course","Mobile"))
@@ -42,6 +55,10 @@ class MainWindow(QMainWindow):
 
     def insert(self):
         dialog = InsertDialog()
+        dialog.exec()
+
+    def search(self):
+        dialog = SearchDialog()
         dialog.exec()
 
 
@@ -86,6 +103,46 @@ class InsertDialog(QDialog):
         cursor.close()
         connection.close()
         sms.load_data()
+
+
+class SearchDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Search Student")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        layout = QVBoxLayout()
+
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        button = QPushButton("Search")
+        button.clicked.connect(self.search_student)
+        layout.addWidget(button)
+
+
+
+        self.setLayout(layout)
+
+    def search_student(self):
+        name = self.student_name.text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        result = cursor.execute("SELECT * FROM students where Name = ?",(name,))
+        rows = list(result)
+        print(rows)
+        items = sms.table.findItems(name, Qt.MatchFlag.MatchFixedString)
+        for item in items:
+            print(item)
+            sms.table.item(item.row(),1).setSelected(True)
+
+        cursor.close()
+        connection.close()
+
+
+
 
 
 app = QApplication(sys.argv)
